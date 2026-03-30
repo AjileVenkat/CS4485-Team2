3c7d1bd8793a751ff472e04fb04e56917230b124
Nguyen P. Do <dophucnguyen2005@gmail.com>
2026-03-30 11:01:50 -0500

    WIP improved matrix and svm

--- feature_matrix.py
+++ feature_matrix.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import numpy as np
+from sklearn.feature_selection import mutual_info_regression
 
-def features_extraction(sample, id):
+def features_extraction(sample, id, md_row):
     # Compute PSD for all 19 channels with the Welch method
     sample_psd = sample.compute_psd(method='welch', fmin=1, fmax=30, verbose=False)
     sample_data, freqs = sample_psd.get_data(return_freqs=True)
@@ -20,19 +21,45 @@ def features_extraction(sample, id):
     # Iterate through all electrodes and create a column name
     feature = {'Subject_ID': id}
     for i, channel in enumerate(sample.ch_names):
+        total_channel_power = sample_data[i, :].sum()
+        band_vals = {}
         for band, mask in power_bands.items():
-            col = f"{channel}_{band}"
-            # Find the mean power for that specific range for feature extraction
-            feature[col] = sample_data[i, mask].mean()
+            mean_power = sample_data[i, mask].mean()
+            band_vals[band] = mean_power
+            feature[f"{channel}_{band}_relative"] = mean_power / total_channel_power
+        if band_vals['Alpha'] > 0:
+            feature[f"{channel}_Theta_Alpha_Ratio"] = band_vals['Theta'] / band_vals['Alpha']
     
+    connective_pairs = [('Fp1', 'Fp2'), ('Fp2', 'F4'), ('Fp2', 'F8'), ('Fp2', 'T4'), ('F3', 'F4'), 
+                        ('T6', 'P4'), ('T4', 'P4'), ('T5', 'P3'), ('T4', 'T6'), ('T3', 'P3'), ('T3', 'T5'), ('O1', 'O2'), ('P3', 'P4')]
+    
+    
+    
+    for c1, c2 in connective_pairs:
+        try:
+            c1_data = sample.get_data(picks = c1)[0].reshape(-1, 1)
+            c2_data = sample.get_data(picks = c2)[0]
+            phi_value = mutual_info_regression(c1_data, c2_data, discrete_features=False, random_state=42)[0]
+            feature[f"Corr_{c1}_{c2}"] = phi_value
+        except ValueError:
+            feature[f"Corr_{c1}_{c2}"] = 0
+    
+    if md_row is not None:
+        feature['Age'] = md_row['Age']
+        feature['Gender'] = 0 if md_row['Gender'] == 'M' else 1
+    else:
+        feature['Age'] = np.nan
+        feature['Gender'] = np.nan
     return feature
 
 if __name__ == "__main__":
     dir = "ds004504"
     rows = []
+    md_df = pd.read_csv(os.path.join(dir, "participants.tsv"), sep='\t').set_index('participant_id')
     for i in sorted(os.listdir(dir)):
         if not i.startswith("sub-"):
             continue
+        
         
         # Load .set files and create an entry per subject
         sub_num = int(i.split('-')[1])
@@ -40,8 +67,8 @@ if __name__ == "__main__":
         if os.path.exists(path):
             print(f"Extracting features for {i}...")
             raw_data = mne.io.read_raw_eeglab(path, preload=True, verbose=False)
-
-            sub_features = features_extraction(raw_data, i)
+            md_data = md_df.loc[i] if i in md_df.index else None
+            sub_features = features_extraction(raw_data, i, md_data)
 
             # Group these entries into 3 groups
             if sub_num <= 36:
--- rf2.py
+++ rf2.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import joblib
 
@@ -14,6 +15,16 @@ dFrame = pd.read_csv("AD_Feature_matrix.csv")
 # Select the features and target
 X = dFrame.drop(columns=['Subject_ID', 'Group'])
 y = dFrame['Group']
+# Splitting train and temporary data (80/20), and split the temporary data into validation and test data (50/50)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+rf = RandomForestClassifier(random_state=42)
+select = RFE(rf, n_features_to_select=5, step=1)
+select = select.fit(X_train, y_train)
+
+X_train_best = select.transform(X_train)
+X_test_best = select.transform(X_test)
+select_names = X.columns[select.support_]
 
 parameters = {
     'n_estimators': [50, 100, 200],
@@ -22,8 +33,7 @@ parameters = {
     'max_features': ['sqrt', 'log2']
 }
 
-# Splitting train and temporary data (80/20), and split the temporary data into validation and test data (50/50)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
 
 print(f"Training size: {len(X_train)}")
 print(f"Testing size: {len(X_test)}")
@@ -31,19 +41,20 @@ print(f"Testing size: {len(X_test)}")
 # Initialize random forest model and fine tuning parameters with a grid search
 random_forest = RandomForestClassifier(random_state=42)
 search = GridSearchCV(estimator=random_forest, param_grid=parameters, cv=5, scoring='accuracy', verbose=1)
-search.fit(X_train, y_train)
+search.fit(X_train_best, y_train)
 
 optimal_random_forest = search.best_estimator_
 skf = StratifiedKFold(n_splits=5)
-cv_scores = cross_val_score(optimal_random_forest, X, y, cv=skf)
+X_best = select.transform(X)
+cv_scores = cross_val_score(optimal_random_forest, X_best, y, cv=skf)
 
 print(f"Optimal Parametesr: {search.best_params_}")
 # Display performance metrics (will add more metrics for further evaluation)
-y_predict = optimal_random_forest.predict(X_test)
+y_predict = optimal_random_forest.predict(X_test_best)
 print(f"Accuracy Score: {accuracy_score(y_test, y_predict):.2%}")
 print("\nClassification Report: ")
 print(classification_report(y_test, y_predict, target_names=['Healthy', 'FTD', 'AD']))
 print(f"Standard Deviation: {cv_scores.std():.2%}")
-
+print(f"Best features: {list(select_names)}")
 joblib.dump(optimal_random_forest, "alzheimers_model.joblib")
 print("Saved model")
