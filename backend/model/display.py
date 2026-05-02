fbefab3cafb90033810db8b1026dfb7437b25377
Nguyen P. Do <69228834+bigcouchman@users.noreply.github.com>
2026-05-02 00:00:00 -0500

    Delete backend/model/display.py

--- backend/model/display.py
+++
@@ -1,9 +0,0 @@
-import mne
-
-eeg_file_raw = r"ds004504\sub-001\eeg\sub-001_task-eyesclosed_eeg.set"
-
-raw_eeg = mne.io.read_raw_eeglab(eeg_file_raw, preload=True)
-
-print(raw_eeg .info)
-
-raw_eeg .plot()
